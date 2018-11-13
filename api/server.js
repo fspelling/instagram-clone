ca7204a3edfbda0a1f48977a59fa9fbc852ab15f
FERNANDO LENTISCO SPELLING <fernandpel@cptm.info>
2018-11-13 10:42:15 -0300

    implementacao api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
+var objectId = require('mongodb').ObjectId;
 
 var app = express();
 
@@ -11,8 +12,93 @@ var port = 8080;
 
 app.listen(port);
 
+var db = new mongodb.Db('instagram', new mongodb.Server('localhost', 27017, {}));
+
 console.log(`servidor http esta executando na porta ${port}`);
 
-app.get('/', function (req, res) {
-    res.send({ msg: 'ola' });
+app.get('/api', function (req, res) {
+    db.open(function (erro, mongoClient) {
+        mongoClient.collection('postagens', function (erro, collection) {
+            collection.find({}).toArray(function (erro, result) {
+                if (erro)
+                    res.json(erro);
+                else
+                    res.json(result);
+
+                mongoClient.close();
+            });
+        });
+    });
+});
+
+app.get('/api/:id', function (req, res) {
+    db.open(function (erro, mongoClient) {
+        mongoClient.collection('postagens', function (erro, collection) {
+            var id = req.params.id;
+
+            collection.find(objectId(id)).toArray(function (erro, result) {
+                if (erro)
+                    res.json(erro);
+                else {
+                    if (result[0] == undefined)
+                        res.status(400).json(result[0]);
+                    else
+                        res.json(result[0]);
+                }
+
+                mongoClient.close();
+            });
+        });
+    });
+});
+
+app.post('/api', function (req, res) {
+    var dados = req.body;
+
+    db.open(function (erro, mongoClient) {
+        mongoClient.collection('postagens', function (erro, collection) {
+            collection.insert(dados, function (erro, records) {
+                if (erro)
+                    res.json(erro);
+                else
+                    res.json('inclusão realizada com sucesso');
+
+                mongoClient.close();
+            });
+        });
+    });
+});
+
+app.put('/api/:id', function (req, res) {
+    db.open(function (erro, mongoClient) {
+        mongoClient.collection('postagens', function (erro, collection) {
+            var id = req.params.id;
+
+            collection.update({ _id: { $eq: objectId(id) } }, { $set: { titulo: req.body.titulo } }, {}, function (erro, records) {
+                if (erro)
+                    res.json(erro);
+                else
+                    res.json(records);
+
+                mongoClient.close();
+            });
+        });
+    });
+});
+
+app.delete('/api/:id', function (req, res) {
+    db.open(function (erro, mongoClient) {
+        mongoClient.collection('postagens', function (erro, collection) {
+            var id = req.params.id;
+
+            collection.remove({ _id: { $eq: objectId(id) } }, true, function (erro, records) {
+                if (erro)
+                    res.json(erro);
+                else
+                    res.json(records);
+
+                mongoClient.close();
+            });
+        });
+    });
 });
